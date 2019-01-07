3b06e8e2abb737e6395842e93fa6b0428f8b4f4b
juliely8619 <july8619@yahoo.com>
2019-01-07 12:01:14 -0800

    set up user input and function calls

--- lab0preworkcalc/Program.cs
+++ lab0preworkcalc/Program.cs
@@ -6,7 +6,44 @@ namespace lab0preworkcalc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //asks the user which what they want to do (add, subtract, multiply, or divide)
+            Console.WriteLine("What would you like to do: Add (+), Subtract (-), Multiply (*) or Divide (/)?");
+            string userCalcChoice = Console.ReadLine();
+            string userCalcChoiceUpper = userCalcChoice.ToUpper();
+
+            //asks the user for their numbers
+            Console.WriteLine($"What is the first number you would like to {userCalcChoice}");
+            int userParamOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"What would you like to {userCalcChoice} against {userParamOne}");
+            int userParamTwo = Convert.ToInt32(Console.ReadLine());
+
+            //if clauses to call which function
+            //I want answer format to be param1 choice param2 = answer
+            if (userCalcChoice == "+" || userCalcChoiceUpper == "ADD") 
+            {
+                int addAnswer = Addition(userParamOne, userParamTwo);
+                Console.WriteLine($"{userParamOne} + {userParamTwo} = {addAnswer}");
+            }
+            else if (userCalcChoice == "-" || userCalcChoiceUpper == "SUBTRACT")
+            {
+                int subAnswer = Subtraction(userParamOne, userParamTwo);
+                Console.WriteLine($"{userParamOne} + {userParamTwo} = {subAnswer}");
+            }
+            else if (userCalcChoice == "*" || userCalcChoiceUpper == "MULTIPLY")
+            {
+                int multAnswer = Multiplication(userParamOne, userParamTwo);
+                Console.WriteLine($"{userParamOne} + {userParamTwo} = {multAnswer}");
+            }
+            else if (userCalcChoice == "/" || userCalcChoiceUpper == "DIVIDE")
+            {
+                int divAnswer = Division(userParamOne, userParamTwo);
+                Console.WriteLine($"{userParamOne} + {userParamTwo} = {divAnswer}");
+            }
+            else //they didn't chose on of my selection, add do while if I have time later to repeat to ask until I get a valid choice
+            {
+                Console.WriteLine($"{userCalcChoice} is not a feature of this calculator");
+            }
+            Console.ReadLine();//so it doesn't auto exit
         }
 
         //add function
